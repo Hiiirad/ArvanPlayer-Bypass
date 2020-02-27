6b5b443cf2018bbb965518a62e42088d1abbcd99
Hirad Rasoolinejad <hirad.rad@gmail.com>
2020-02-27 23:44:03 +0330

    Uploaded bash scripts

---
+++ concatenate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -n "cat "
+for i in {1..491}; do
+	echo -n "seg-$i-f5-v1-a1.ts "
+done
---
+++ link-maker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in {1..392}; do
+	echo "https://memoryleak.arvanvod.com/kb7yZJyN0g/PeAWr4vmMl/h_,144_200,240_400,360_800,480_1500,720_2500,1080_4366,k.mp4.list/seg-$i-f5-v1-a1.ts"
+done
